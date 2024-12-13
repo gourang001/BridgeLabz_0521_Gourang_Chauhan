b25b3ac846b853ee62e0599d91034e93003fc7bc
gourang001 <gourangchauhan001@gmail.com>
2024-12-13 13:46:05 +0530

    [Gourang] ADD :Added new files

---
+++ classes_object.js
@@ -0,0 +1,20 @@
+// A class in JavaScript is a blueprint for creating objects with shared properties and methods
+// An object is an instance of a class. It represents a specific entity with unique data
+
+
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+
+    greet(){
+        console.log(`hi my name is ${this.name} and i am ${this.age} years old`);
+    }
+}
+
+let person1 = new Person("Alice", 25); // creating objects
+let person2 = new Person("Bob", 30);   
+
+person1.greet(); 
+person2.greet(); 
